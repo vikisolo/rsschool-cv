2a58a996212088d55cd2fee78539e18bad7547f4
viki <150080598+vikisolo@users.noreply.github.com>
2024-09-12 13:22:20 +0300

    rrrr scripts.js

--- shelter/scripts.js
+++ shelter/scripts.js
@@ -30,39 +30,3 @@ popups.onclick = function (event) {
     }
 }
 
-
-const dialog = document.getElementById('myDialog')
-const dialogOpener = document.querySelector('.openDialogBtn')
-const dialogCloser = dialog.querySelector('.closeDialogBtn')
-
-function closeOnBackDropClick({ currentTarget, target }) {
-  const dialog = currentTarget
-  const isClickedOnBackDrop = target === dialog
-  if (isClickedOnBackDrop) {
-    close()
-  }
-}
-
-function openModalAndLockScroll() {
-  dialog.showModal()
-  document.body.classList.add('scroll-lock')
-}
-
-function returnScroll() {
-  document.body.classList.remove('scroll-lock')
-}
-
-function close() {
-  dialog.close()
-  returnScroll()
-}
-
-dialog.addEventListener('click', closeOnBackDropClick)
-dialog.addEventListener('cancel', (event) => {
-  returnScroll()
-});
-dialogOpener.addEventListener('click', openModalAndLockScroll)
-dialogCloser.addEventListener('click', (event) => {
-  event.stopPropagation()
-  close()
-})
